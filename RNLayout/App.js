4f02dd700996daf0b2efaa964eb85665183f6ea1
lensaBayu <bkprasetya@gmail.com>
2024-02-21 14:24:58 +0700

    Add new folder

--- RNLayout/App.js
+++ RNLayout/App.js
@@ -4,9 +4,9 @@ import Box from './components/Box';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "black" }} >TES</Box>
-      <Box style={{ backgroundColor: "blue" }} >Box 2</Box>
-      <Box style={{ backgroundColor: "green" }} >Box 3</Box>
+      <Box style={{ backgroundColor: "black", alignSelf: "flex-start" }} >Box 1</Box>
+      <Box style={{ backgroundColor: "blue", alignSelf: "flex-end" }} >Box 2</Box>
+      <Box style={{ backgroundColor: "green", alignSelf: "center" }} >Box 3</Box>
       <Box style={{ backgroundColor: "yellow" }} >Box 4</Box>
       <Box style={{ backgroundColor: "red" }} >Box 5</Box>
       <Box style={{ backgroundColor: "purple" }} >Box 6</Box>
@@ -17,6 +17,12 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 64
+    flex: 1,
+    // flexDirection: "row",
+    // justifyContent: "center",
+    // alignItems: "flex-start", 
+    marginTop: 64,
+    borderWidth: 6,
+    borderColor: "red"
   },
 });
